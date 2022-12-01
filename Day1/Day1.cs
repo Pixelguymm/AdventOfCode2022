6e611d590c6e83b23acd7a53cfb0216ff21cbf6a
Pixelguymm <maartenmarx@hotmail.be>
2022-12-01 12:53:58 +0100

    Day 1

---
+++ Day1/Day1.cs
@@ -0,0 +1,14 @@
+﻿var groups = File.ReadAllText("Input.txt")
+    .Trim()
+    .Split("\n\n")
+    .Select(x =>
+        x.Split("\n").Select(int.Parse)
+    );
+
+var calories = groups.Select(g => g.Sum()).ToList();
+
+// 1
+Console.WriteLine(calories.Max());
+
+// 2
+Console.WriteLine(calories.OrderByDescending(x => x).Take(3).Sum());
