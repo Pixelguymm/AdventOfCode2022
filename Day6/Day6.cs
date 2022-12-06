f701d46eb4c5082c18b680525713a11222bfab51
Pixelguymm <maartenmarx@hotmail.be>
2022-12-06 08:42:26 +0100

    Day 6

---
+++ Day6/Day6.cs
@@ -0,0 +1,23 @@
+﻿var chars = File.ReadAllText("Input.txt").Trim().ToList();
+
+// 1
+Console.WriteLine(chars.FindMark(4));
+
+// 2
+Console.WriteLine(chars.FindMark(14));
+
+static class Extensions
+{
+    public static int FindMark(this List<char> characters, int length)
+    {
+        for (var i = 0; i <= characters.Count; i++)
+        {
+            if (characters.GetRange(i, length).Distinct().Count() == length)
+            {
+                return i + length;
+            }
+        }
+
+        return 0;
+    }
+}
